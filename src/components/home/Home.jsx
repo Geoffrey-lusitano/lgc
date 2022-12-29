a099617bd878bba865276e6085e0053e20751ea4
GeoCITW <g.bl@outlook.fr>
2022-12-29 14:48:50 +0100

    css home page

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,45 +1,62 @@
 import React from "react";
 import './home.css';
 import Slider from './Slider.jsx';
-import { BiBuildings } from "react-icons/bi";
+import { BsTelephoneFill } from "react-icons/bs";
+import { BsWhatsapp } from "react-icons/bs";
+import { MdEmail } from "react-icons/md";
 
 const Home = () => {
     return (
       <section id="home">
         <div className="home__general">
+          <h1>La solution informatique complète à Chambéry</h1>
+          <div className="under__title"></div>
           <div className="carousel">
             <Slider />
           </div>
-          <h1>Solution complete informatiques Chambéry</h1>
-          <p>
-            Un projet web ou une évolution de votre infrastructure ? LGC vous
-            accompagne de A - Z dans vos projets IT. Dépannage informatique pour
-            les particuliers et professionnels. Créations de sites webs
-            sur-mesures. Maison connecté - Formation - Assemblage PC unique.
-            Forte de collaborateurs experimenté nous garantissont une approche
-            qui garantira la reussite de votre projet
+          <h2 className="slogan">
+            Un projet web ou une évolution de votre infrastructure ?
+            <br />
+            LGC vous accompagne de A - Z 
+            <br />
+            dans vos projets IT.
+          </h2>
+          <br />
+          <p className="home__text__center">
+            Dépannage informatique pour les particuliers et professionnels.
+            <br />
+            Créations de sites webs sur-mesures. Maison connecté - Formation -
+            Assemblage PC unique. Forte de collaborateurs experimenté nous
+            garantissont une approche qui garantira la reussite de votre projet
           </p>
-          <h2>Nous contacter</h2>
+          <h2 className="h2__contact">Nous contacter</h2>
           <div className="home__contact">
             <div className="contact__cels">
               <h3>Une panne ?</h3>
               <div className="cels__content">
-                <BiBuildings />
+                <BsTelephoneFill className="cels__icon" />
                 <p> 06 65 10 34 78</p>
               </div>
             </div>
             <div className="contact__cels">
               <h3>Un projet ?</h3>
               <div className="cels__content">
-                <BiBuildings />
-                <p>contact@lgc.fr</p>
+                <MdEmail className="cels__icon" />
+                <a href="mailto:contact@lgcorporation.fr" target="_blank">
+                contact@lgcorporation.fr
+              </a>
               </div>
             </div>
             <div className="contact__cels">
               <h3>Un conseil ?</h3>
               <div className="cels__content">
-                <BiBuildings />
-                <p>+33 6 65 10 34 78</p>
+                <BsWhatsapp className="cels__icon" />
+                <a
+                href="https://api.whatsapp.com/send?phone=33665103478"
+                target="_blank"
+              >
+                +33 6 65 10 34 78
+              </a>
               </div>
             </div>
           </div>
